e3da6c354e04e3f3e82bdf467cb2b88ad62707f2
Grant Zheng <Grant Zheng@DESKTOP-D6N497R>
2018-12-11 23:42:19 -0500

    Started the controller class

--- ControlPorject/ImprovingVersion/controller.py
+++ ControlPorject/ImprovingVersion/controller.py
@@ -9,6 +9,24 @@ import control
 from scipy.ndimage import gaussian_filter1d
 from util import *
 
-def controller():
-    pass
-    return 0
+class Controller:
+    
+    def __init__ (self, traj, vehicle, vD):
+        self.traj = traj
+        self.vehicle = vehicle
+        self.lookForward = 25
+    
+    def calculateABMatrix(self):
+        lr = self.vehicle.lr
+        lf = self.vehicle.lf
+        Ca = self.vehicle.Ca
+        Iz = self.vehicle.Iz
+        f  = self.vehicle.f
+        m  = self.vehicle.m
+        
+    def calucalteCurvatureK(self):
+        firstDer = gaussian_filter1d(self.traj, 1)
+        secondDer = gaussian_filter1d(self.traj, 2)
+            
+    def getController(self):
+        #TODO: Return controller parameters for the control
